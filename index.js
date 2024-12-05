9e645038b904712a31e5951faec36531269e7f3c
NullifyCrescent9202 <97800784+NullifyCrescent9202@users.noreply.github.com>
2024-12-04 22:25:35 -0500

    Create html page and operate function.

--- index.js
+++ index.js
@@ -13,3 +13,22 @@ function multiply(a, b){
 function divide(a, b){
     return a / b;
 }
+
+let preNum  = null;
+
+let currentNum = null;
+
+let operand = [1, 2, 3, 4] 
+
+function operate(num1, num2, op){
+    if(op === 1){
+        return add(num1, num2);
+    } else if(op === 2){
+        return subtract(num1, num2);
+    } else if(op === 3){
+        return multiply(num1, num2);
+    } else if (op === 4){
+        return divide(num1, num2);
+    }
+}
+
